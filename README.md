fd89eb027dfed27ef88c41855a5eec10d9399185
Alejandro Sanchez <aalejo@gmail.com>
2018-05-13 04:07:02 +0000

    preview

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PokerSociety.com Mobile Version
 
+
+For hostgator ~/.bashrc
+```
+alias php='/opt/php71/bin/php'
+alias composer='/opt/php71/bin/php /opt/cpanel/composer/bin/composer'
+alias wp='/opt/php71/bin/php /usr/local/bin/wp'
+export DRUSH_PHP='/opt/php71/bin/php'
+```
--- public/index.php
+++ public/index.php
@@ -7,22 +7,25 @@
     <link rel="shortcut icon" href="/poker-icon.ico"></head>
     <body>
     <?php 
-        $content = @file_get_contents('http://admin.thepokersociety.com/wp-json/ps/v1/settings');
-        if($content)
+        if(!isset($_GET['preview']))
         {
-            $settings = json_decode($content);
-            if(!empty($settings)){
-                if($settings->mantainance_mode=='active'){
-                    ?>
-                	<div>
-                    	<p></p>
-                    	<p style="text-align: center;"><img class="aligncenter wp-image-36 size-medium" src="pokersociety.png" alt="" width="300" height="229"></p>
-                        <h1 style="text-align: center;">Website Under Maintenance</h1>
-                        <p style="text-align: center;">Our Website is currently undergoing scheduled maintenance. Please check back soon.</p><p></p>
-                	</div>
-                	</body></html>
-                	<?php
-                	die();
+            $content = @file_get_contents('http://admin.thepokersociety.com/wp-json/ps/v1/settings');
+            if($content)
+            {
+                $settings = json_decode($content);
+                if(!empty($settings)){
+                    if($settings->mantainance_mode=='active'){
+                        ?>
+                    	<div>
+                        	<p></p>
+                        	<p style="text-align: center;"><img class="aligncenter wp-image-36 size-medium" src="pokersociety.png" alt="" width="300" height="229"></p>
+                            <h1 style="text-align: center;">Website Under Maintenance</h1>
+                            <p style="text-align: center;">Our Website is currently undergoing scheduled maintenance. Please check back soon.</p><p></p>
+                    	</div>
+                    	</body></html>
+                    	<?php
+                    	die();
+                    }
                 }
             }
         }
