2596d1d7005f186f046732d3efaff36e8aad974b
Alejandro Sanchez <aalejo@gmail.com>
2018-05-13 03:44:56 +0000

    mantainance mode

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>The Poker Society</title>
+    <link rel="shortcut icon" href="/poker-icon.ico"></head>
+    <body>
+    <?php 
+        $content = @file_get_contents('https://admin.thepokersociety.com/wp-json/ps/v1/settings');
+        if($content)
+        {
+            $settings = json_decode($content);
+            if(!empty($settings)){
+                if($settings->mantainance_mode=='active'){
+                    ?>
+                	<div>
+                    	<p></p>
+                    	<p style="text-align: center;"><img class="aligncenter wp-image-36 size-medium" src="pokersociety.png" alt="" width="300" height="229"></p>
+                        <h1 style="text-align: center;">Website Under Maintenance</h1>
+                        <p style="text-align: center;">Our Website is currently undergoing scheduled maintenance. Please check back soon.</p><p></p>
+                	</div>
+                	</body></html>
+                	<?php
+                	die();
+                }
+            }
+        }
+    ?>
+        <div id="app"></div>
+        <script type="text/javascript" src="/bundle.js"></script>
+    </body>
+</html>
